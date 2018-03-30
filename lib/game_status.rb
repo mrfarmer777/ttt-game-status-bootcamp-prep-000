88c30fdc950faf452e6fa34bb7b93174edbf2c0d
Matt Farmer <mrfarmer777@gmail.com>
2018-03-30 22:45:58 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,39 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
+
+def won?(board)
+
+  won = WIN_COMBINATIONS.detect do |combo|
+    all_full = combo.all? do |position|
+      position_taken?(board,position.to_i)
+    end
+    if(all_full)
+      if(board[combo[0]]==board[combo[1]] && board[combo[0]]==board[combo[2]])
+        combo
+      end
+    end
+  end
+  if(won==[])
+    false
+  else
+    won
+  end
+end
+
+
+def full?(board)
+  full = board.all? do |position|
+    (!position==" " && !position=="")
+  end
+end
